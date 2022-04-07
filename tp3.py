bdddf950e2233292778ed161a2863cbbb3fbcabb
PriscilleMUTOBA <mukoka.mutoba@stud.fils.upb.ro>
2022-04-07 15:16:56 +0300

    Create tp3.py

---
+++ tp3.py
@@ -0,0 +1,26 @@
+"""
+var1 =12
+var2= 17.9
+var3 = "SdE2"
+print ("type of", var1 ,"is", type(var1))
+print("type of", var2, "is", type(var2))
+print("type of", var3, "is", type(var3))
+
+"""
+"""
+#EX2
+n=16
+global v3   
+for i in range(1,n):
+    v3 = i % 2
+    if v3 !=0:
+     print(i)
+    if i >= n:
+        break
+
+#EX3
+listefruits:["mangue","orange","banane","anana","pomme"]
+for i in range(0,len(listefruits)):
+    global v4=
+
+    """
